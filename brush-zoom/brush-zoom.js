68213201b02fbcda61d71017c20e91ebd3344f61
xht <531026360@qq.com>
2019-09-17 19:08:58 +0800

    brush-zoom

---
+++ brush-zoom/brush-zoom.js
@@ -0,0 +1,11 @@
+var width = 1000,
+    height = 500;
+var padding = { top: 50, left: 50, bottom: 50, right: 50 };
+
+var svg = d3.select('body')
+    .append('svg')
+    .attr('width', width + padding.left + padding.right)
+    .attr('height', height + padding.top + padding.bottom)
+
+
+
