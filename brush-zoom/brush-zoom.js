122466d530a10d939d6bc5134b3bcd0df2dca02f
xht <531026360@qq.com>
2019-09-18 19:04:40 +0800

    bursh-zoom

--- brush-zoom/brush-zoom.js
+++ brush-zoom/brush-zoom.js
@@ -1,5 +1,7 @@
 var width = 1000,
-    height = 500;
+    height = 500,
+    height1 = 350,
+    height2 = 100;
 var padding = { top: 50, left: 50, bottom: 50, right: 50 };
 
 var svg = d3.select('body')
@@ -7,5 +9,5 @@ var svg = d3.select('body')
     .attr('width', width + padding.left + padding.right)
     .attr('height', height + padding.top + padding.bottom)
 
-
+var parseDate = d3.timeParse("%b %Y");
 
